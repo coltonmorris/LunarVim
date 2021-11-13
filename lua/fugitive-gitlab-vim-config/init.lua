203e0752936b0df7ca5ee565b0bc0a5f24718d33
Colton Morris <colton.morris@tcnbroadcasting.com>
2021-11-12 20:22:39 -0700

    ya boiii

--- lua/fugitive-gitlab-vim-config/init.lua
+++
@@ -1,2 +0,0 @@
--- for private gitlab domains
-vim.g.fugitive_gitlab_domains = {'http://git', 'http://git.tcncloud.net'}
--- lua/project-nvim-config/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.nvim_tree_respect_buf_cwd = 1
-
-require("project_nvim").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
